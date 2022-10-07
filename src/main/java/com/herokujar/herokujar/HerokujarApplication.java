26c0dc3a91b54585b08b41a3d650ad22b8a100e3
Juhani Koski <juhkukoskii@gmail.com>
2022-10-07 20:30:44 +0300

    First

---
+++ src/main/java/com/herokujar/herokujar/HerokujarApplication.java
@@ -0,0 +1,13 @@
+package com.herokujar.herokujar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokujarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokujarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/herokujar/herokujar/HerokujarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herokujar.herokujar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokujarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
